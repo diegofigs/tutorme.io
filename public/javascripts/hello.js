ef26871bd0ed921fd41df3d90d7d371b36657412
Diego Figueroa <diego.fv94@gmail.com>
2016-10-03 15:56:16 -0400

    serve static Materialize starter template

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,11 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
