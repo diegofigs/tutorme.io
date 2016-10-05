41cb9e347e99b3f31ff8329ff4d1963ad7c9a7f6
Diego Figueroa <diego.fv94@gmail.com>
2016-10-04 21:32:52 -0400

    add register methods

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,14 +1,19 @@
 package controllers;
 
+import models.User;
+import play.data.FormFactory;
 import play.mvc.*;
-
+import play.data.Form;
 import views.html.*;
+
+import javax.inject.Inject;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
 public class HomeController extends Controller {
+    @Inject FormFactory formFactory;
 
     /**
      * An action that renders an HTML page with a welcome message.
@@ -18,6 +23,16 @@ public class HomeController extends Controller {
      */
     public Result index() {
         return ok(index.render());
+    }
+
+    public Result getRegister() {
+        return ok(register.render());
+    }
+
+    public Result postRegister() {
+        Form<User> userForm = formFactory.form(User.class);
+        User user = userForm.bindFromRequest().get();
+        return ok("Hello " + user.getFirstName() + " " + user.getLastName());
     }
 
 }
