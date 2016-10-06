a72d48e22309de55ef7d8aa886789174c52feb1f
Diego Figueroa <diego.fv94@gmail.com>
2016-10-05 23:44:24 -0400

    change from list to hashMap for random access, add auth methods

--- app/models/User.java
+++ app/models/User.java
@@ -1,20 +1,60 @@
 package models;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.data.validation.Constraints;
+import play.libs.Json;
+import play.mvc.Result;
 
+import javax.inject.Singleton;
 import javax.validation.Constraint;
+import java.util.*;
+
+import static play.mvc.Http.Context.Implicit.session;
+import static play.mvc.Results.ok;
 
 /**
  * Created by diegofigs on 10/4/16.
  */
 public class User {
 
-    protected String firstName;
-    protected String lastName;
+    @Singleton
+    private static Map<String, User> userMap = new HashMap<>();
+
+    private String firstName;
+    private String lastName;
 
     @Constraints.Required
-    protected String email;
-    protected String password;
+    private String email;
+    private String password;
+
+    public static boolean create(Form<User> userForm) {
+        User user = userForm.bindFromRequest().get();
+        for(Map.Entry<String, User> u: userMap.entrySet()){
+            if(u.getValue().getEmail().equals(user.getEmail())){
+                return false;
+            }
+        }
+        userMap.put(user.getEmail(), user);
+        session().put("email", user.getEmail());
+        session().put("name", user.getFirstName());
+        return true;
+    }
+
+    public static boolean authenticate(String email, String password) {
+        for(Map.Entry<String, User> u: userMap.entrySet()){
+            if(u.getValue().getEmail().equals(email) && u.getValue().getPassword().equals(password))
+                session().put("email", u.getValue().getEmail());
+                session().put("name", u.getValue().getFirstName());
+                return true;
+        }
+        return false;
+    }
+
+    public static User get(String email) {
+        return userMap.get(email);
+    }
 
     public String getEmail() {
         return email;
