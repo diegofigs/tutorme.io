77e6115e95d5721e230f9ab8cb542247117989ac
Luis Rivera <luis.rivera157@upr.edu>
2016-10-14 05:22:09 -0400

    Improved modals on lesson view, implemented the videos list, and assignments and documents with angular

--- public/javascripts/controllers/lessons.js
+++ public/javascripts/controllers/lessons.js
@@ -11,13 +11,22 @@
  */
 angular.module('publicApp').controller('LessonsCtrl',
             function ($scope, auth, lessonsService, $sce) {
-
-
+                var lessons = lessonsService.getLessons();
+                $scope.activeLesson=lessons[0];
                 $scope.lessons = lessonsService.getLessons();
-
+                $scope.getActiveLesson = function(){
+                    return $scope.activeLesson;
+                };
+                $scope.setActiveLesson = function(name){
+                    $scope.activeLesson = lessonsService.getLesson(name);
+                };
                 $scope.trustSrc = function(videoURL) {
                     return $sce.trustAsResourceUrl(videoURL);
                 }
+                $scope.modalDetails = function(assign){
+                    $scope.assign = assign;
+                    $('#modalDetails').openModal();
+                };
 
                 $scope.insertLesson = function(){
                     //To be implemented in next phase
@@ -27,9 +36,19 @@ angular.module('publicApp').controller('LessonsCtrl',
                     //To be implemented in next phase
                 };
 
+                $scope.getIframeSrc = function(src) {
+                    return 'https://www.youtube.com/embed/' + src;
+                };
+
                 $scope.insertAssignmentInLesson = function(){
                     //To be implemented in next phase
                 };
+
+                $scope.$watch('lessons[0].open', function(isOpen){
+                    if (isOpen) {
+                        console.log('First lesson was opened');
+                    }
+                });
 
 
             }
--- public/javascripts/services/lessonsService.js
+++ public/javascripts/services/lessonsService.js
@@ -12,12 +12,41 @@ angular.module('publicApp').service('lessonsService', function () {
             id:0,
             name: 'Limits',
             videos: [
-                {vidTitle:'Video 1', videoURL:''},
-                {vidTitle:'Video 2', videoURL:''},
-                {vidTitle:'Video 3', videoURL:''}
+                {vidTitle:'Vid 1', src:'Q8TXgCzxEnw'},
+                {vidTitle:'Vid 2', src:'Q8TXgCzxEnw'},
+                {vidTitle:'Vid 3', src:'Q8TXgCzxEnw'}
             ],
             assignments:[
-                {asTitle:'Exercises', dueDate: '10/10/16 11:59 pm', attachmentURL:'C:\Users\luisr\OneDrive\Uni\Resume', grade:null, description:'There are 5 exercises. if you have at least 2 wrong answers you will get a 0'}
+                {
+                    asId: 0,
+                    asTitle: 'Exercises',
+                    dueDate: '10/19/16 11:59 pm',
+                    attachmentURL: 'C:\Users\luisr\OneDrive\Uni\Resume',
+                    description: 'There are 5 exercises. if you have at least 2 wrong answers you will get a 0',
+                    submissions: [
+                        {
+                            sId: 1,
+                            sName: 'User 1',
+                            grade: null,
+                            dateSubmitted: '10/14/16 11:05 am',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        },
+                        {
+                            sId: 2,
+                            sName: 'User 2',
+                            grade: null,
+                            dateSubmitted: '10/15/16 1:15 pm',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        },
+                        {
+                            sId: 3,
+                            sName: 'User 3',
+                            grade: null,
+                            dateSubmitted: '10/18/16 10:45 pm',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        }
+                    ]
+                }
             ],
             documents: [
                 {docTitle:'Presentation', docURL:'C:\Users\luisr\OneDrive\Uni\Resume', docDescription:'This is the presentation used in the videos.'}
@@ -27,39 +56,140 @@ angular.module('publicApp').service('lessonsService', function () {
             id:1,
             name: 'Derivatives',
             videos: [
-                {vidTitle:'Video 1', videoURL:'https://www.youtube.com/watch?v=Q8TXgCzxEnw'}
+                {vidTitle:'V1', src:'Q8TXgCzxEnw'}
             ],
             assignments:[
-                {asTitle:'Exercises', dueDate: '10/15/16 11:59 pm', attachmentURL:'C:\Users\luisr\OneDrive\Uni\Resume', grade:null, description:'There are 5 exercises. if you have at least 2 wrong answers you will get a 0'}
+                {
+                    asId:1,
+                    asTitle: 'Exercises',
+                    dueDate: '10/15/16 11:59 pm',
+                    attachmentURL: 'C:\Users\luisr\OneDrive\Uni\Resume',
+                    description: 'There are 5 exercises. if you have at least 2 wrong answers you will get a 0',
+                    submissions: [
+                        {
+                            sId: 1,
+                            sName: 'User 1',
+                            grade: null,
+                            dateSubmitted: '10/14/16 11:05 am',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        },
+                        {
+                            sId: 2,
+                            sName: 'User 2',
+                            grade: null,
+                            dateSubmitted: '10/15/16 1:15 pm',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        },
+                        {
+                            sId: 3,
+                            sName: 'User 3',
+                            grade: null,
+                            dateSubmitted: '10/18/16 10:45 pm',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        }
+                    ]
+                }
             ],
             documents: [
-                {docTitle:'Presentation', docURL:'C:\Users\luisr\OneDrive\Uni\Resume', docDescription:'This is the presentation used in the video.'}
+                {
+                    docTitle: 'Presentation',
+                    docURL: 'C:\Users\luisr\OneDrive\Uni\Resume',
+                    docDescription: 'This is the presentation used in the video.'
+                }
             ]
         },
         {
             id:2,
             name: 'Antiderivatives',
             videos: [
-                {vidTitle:'Video 1', videoURL:'https://www.youtube.com/watch?v=Q8TXgCzxEnw'}
+                {
+                    vidTitle: 'Video 1', src: 'Q8TXgCzxEnw'
+                }
             ],
             assignments:[
-                {asTitle:'Exercises', dueDate: '10/17/16 11:59 pm', attachmentURL:'C:\Users\luisr\OneDrive\Uni\Resume', grade:null, description:'There are 5 exercises. if you have at least 2 wrong answers you will get a 0'}
+                {
+                    asId:2,
+                    asTitle: 'Exercises',
+                    dueDate: '10/17/16 11:59 pm',
+                    attachmentURL: 'C:\Users\luisr\OneDrive\Uni\Resume',
+                    description: 'There are 5 exercises. if you have at least 2 wrong answers you will get a 0',
+                    submissions: [
+                        {
+                            sId: 1,
+                            sName: 'User 1',
+                            grade: null,
+                            dateSubmitted: '10/14/16 11:05 am',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        },
+                        {
+                            sId: 2,
+                            sName: 'User 2',
+                            grade: null,
+                            dateSubmitted: '10/15/16 1:15 pm',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        },
+                        {
+                            sId: 3,
+                            sName: 'User 3',
+                            grade: null,
+                            dateSubmitted: '10/18/16 10:45 pm',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        }
+                    ]
+                }
             ],
             documents: [
-                {docTitle:'Presentation', docURL:'C:\Users\luisr\OneDrive\Uni\Resume', docDescription:'This is the presentation used in the video.'}
+                {
+                    docTitle: 'Presentation',
+                    docURL: 'C:\Users\luisr\OneDrive\Uni\Resume',
+                    docDescription: 'This is the presentation used in the video.'
+                }
             ]
         },
         {
             id:3,
             name: 'Definite Integral',
             videos: [
-                {vidTitle:'Video 1', videoURL:'https://www.youtube.com/watch?v=Q8TXgCzxEnw'}
+                {vidTitle:'Vide1', src:'Q8TXgCzxEnw'}
             ],
             assignments:[
-                {asTitle:'Exercises', dueDate: '10/20/16 11:59 pm', attachmentURL:'C:\Users\luisr\OneDrive\Uni\Resume', grade:null, description:'There are 5 exercises. if you have at least 2 wrong answers you will get a 0'}
+                {
+                    asId:3,
+                    asTitle: 'Exercises',
+                    dueDate: '10/20/16 11:59 pm',
+                    attachmentURL: 'C:\Users\luisr\OneDrive\Uni\Resume',
+                    description: 'There are 5 exercises. if you have at least 2 wrong answers you will get a 0',
+                    submissions: [
+                        {
+                            sId: 1,
+                            sName: 'User 1',
+                            grade: null,
+                            dateSubmitted: '10/14/16 11:05 am',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        },
+                        {
+                            sId: 2,
+                            sName: 'User 2',
+                            grade: null,
+                            dateSubmitted: '10/15/16 1:15 pm',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        },
+                        {
+                            sId: 3,
+                            sName: 'User 3',
+                            grade: null,
+                            dateSubmitted: '10/18/16 10:45 pm',
+                            attachment: 'C:\Users\luisr\OneDrive\Uni\Resume'
+                        }
+                    ]
+                }
             ],
             documents: [
-                {docTitle:'Presentation', docURL:'C:\Users\luisr\OneDrive\Uni\Resume', docDescription:'This is the presentation used in the video.'}
+                {
+                    docTitle: 'Presentation',
+                    docURL: 'C:\Users\luisr\OneDrive\Uni\Resume',
+                    docDescription: 'This is the presentation used in the video.'
+                }
             ]
         }
     ];
