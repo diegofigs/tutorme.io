95c31152c7adc968a0362e2ec6330dda34efafec
Diego Figueroa <diego.fv94@gmail.com>
2016-11-12 23:14:34 -0400

    evolve db to persiste Students and Tutors

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,31 @@
+# --- Add Students and Tutors schema
+
+# --- !Ups
+
+CREATE TABLE students (
+  id INTEGER NOT NULL REFERENCES users(id),
+  summary TEXT,
+  interests TEXT,
+  activities TEXT,
+  skills TEXT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE tutors (
+  id INTEGER NOT NULL REFERENCES users(id),
+  summary TEXT,
+  interests TEXT,
+  education TEXT,
+  certifications TEXT,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO tutors (id)
+    VALUES (1), (2), (3), (4);
+
+# --- !Downs
+
+DROP TABLE students;
+
+DROP TABLE tutors;
+
