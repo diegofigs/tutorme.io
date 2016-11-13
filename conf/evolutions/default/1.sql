4858438b1ca57d29dc88ea65d08bba9908d0a0ee
Diego Figueroa <diego.fv94@gmail.com>
2016-11-12 23:10:55 -0400

    add type field

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -8,13 +8,15 @@ CREATE TABLE users (
   lastname VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
+  type INTEGER NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO users (firstname, lastname, email, password)
-VALUES ('Diego', 'Figueroa', 'diego.fv94@gmail.com', 'diegofigs'),
-  ('Andres', 'Hernandez', 'andres.hernandez2@upr.edu', 'andresher'),
-  ('Luis', 'Rivera', 'luis.rivera157@upr.edu', 'lgrs05');
+INSERT INTO users (firstname, lastname, email, password, type)
+VALUES ('Diego', 'Figueroa', 'diego.fv94@gmail.com', 'diegofigs', 0),
+  ('Andres', 'Hernandez', 'andres.hernandez2@upr.edu', 'andresher', 0),
+  ('Luis', 'Rivera', 'luis.rivera157@upr.edu', 'lgrs05', 0),
+  ('Manuel', 'Rodriguez', 'manuel.rodriguez7@upr.edu', 'manuelrodz', 0);
 
 # --- !Downs
 
