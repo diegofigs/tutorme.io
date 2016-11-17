b54ff99a6ab4f1a38afa00564ed2b009b14790c4
Luis Rivera <luis.rivera157@upr.edu>
2016-11-17 04:31:18 -0400

    Renamed sql files

--- conf/evolutions/default/7.sql
+++ conf/evolutions/default/7.sql
@@ -1,21 +1,62 @@
-# --- Lessons schemas
+# --- Documents, Assignments, and videos schemas
 
 # --- !Ups
 
-CREATE TABLE lessons (
-  lid SERIAL NOT NULL,
-  name VARCHAR(255) NOT NULL,
+CREATE TABLE documents (
+  did SERIAL NOT NULL,
+  dtitle VARCHAR(255) NOT NULL,
+  ddescription VARCHAR(255) NOT NULL,
+  dpath VARCHAR(255) NOT NULL,
+  lid INT NOT NULL,
 
-  PRIMARY KEY (lid)
+
+  FOREIGN KEY (lid) REFERENCES lessons(lid),
+  PRIMARY KEY (did)
 );
 
-INSERT INTO lessons (name)
-VALUES ('Limits'),
-  ('Derivatives'),
-  ('Antiderivatives'),
-  ('Definite Integrals');
+CREATE TABLE assignments (
+  aid SERIAL NOT NULL,
+  atitle VARCHAR(255) NOT NULL,
+  adescription VARCHAR(255) NOT NULL,
+  apath VARCHAR(255) NOT NULL,
+  lid INT NOT NULL,
+  deadline DATE NOT NULL,
+
+
+  FOREIGN KEY (lid) REFERENCES lessons(lid),
+  PRIMARY KEY (aid)
+);
+
+CREATE TABLE videos (
+  vid SERIAL NOT NULL,
+  vtitle VARCHAR(255) NOT NULL,
+  URL VARCHAR(255) NOT NULL,
+  lid INT NOT NULL,
+
+
+  FOREIGN KEY (lid) REFERENCES lessons(lid),
+  PRIMARY KEY (vid)
+);
+
+INSERT INTO documents (dtitle, ddescription, lid, dpath)
+VALUES ('Limits Practice', '15 limits exercises', 1, 'C:\Users\luisr\OneDrive\Uni\Database\Repo\ER Diagram'),
+  ('Derivatives Practice', '15 derivatives exercises', 2, 'C:\Users\luisr\OneDrive\Uni\Database\Repo\ER Diagram'),
+  ('Antiderivatives Practice', '15 antiderivatives exercises', 3, 'C:\Users\luisr\OneDrive\Uni\Database\Repo\ER Diagram'),
+  ('Definite Integrals Practice', '15 definite integrals exercises', 4, 'C:\Users\luisr\OneDrive\Uni\Database\Repo\ER Diagram');
+
+  INSERT INTO assignments (atitle, adescription, lid, deadline, apath)
+VALUES ('Limits Practice', '15 limits exercises', 1, CURRENT_DATE, 'C:\Users\luisr\OneDrive\Uni\Database\Repo\ER Diagram' ),
+  ('Derivatives Practice', '15 derivatives exercises', 2, CURRENT_DATE,'C:\Users\luisr\OneDrive\Uni\Database\Repo\ER Diagram' ),
+  ('Antiderivatives Practice', '15 antiderivatives exercises', 3, CURRENT_DATE, 'C:\Users\luisr\OneDrive\Uni\Database\Repo\ER Diagram' ),
+  ('Definite Integrals Practice', '15 definite integrals exercises', 4, CURRENT_DATE, 'C:\Users\luisr\OneDrive\Uni\Database\Repo\ER Diagram' );
+
+  INSERT INTO videos (vtitle, URL, lid)
+VALUES ('Limits', 'Q8TXgCzxEnw', 1),
+  ('Derivatives', 'Q8TXgCzxEnw', 2),
+  ('Antiderivatives', 'Q8TXgCzxEnw', 3),
+  ('Definite Integrals', 'Q8TXgCzxEnw', 4);
+
 
 # --- !Downs
 
-DROP TABLE lessons;
-
+DROP TABLE documents, assignments, videos;
