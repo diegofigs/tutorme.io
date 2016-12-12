9b4a438370fb5986a084a05604aad17d8b186e4d
Andres Hernandez <andres.hernandez2@upr.edu>
2016-12-12 17:51:48 -0400

    don't let user register with an existing email

--- public/javascripts/controllers/auth.js
+++ public/javascripts/controllers/auth.js
@@ -14,13 +14,27 @@ angular.module('publicApp')
         '$location',
         '$routeParams',
         'auth',
-        function ($scope, $route, $location, $routeParams, auth) {
+        'mailbox',
+        function ($scope, $route, $location, $routeParams, auth, mailbox) {
             $scope.user = {};
+            $scope.userExists = false;
             $scope.createUser = function(){
-                if(!$scope.user.email || !$scope.user.password){ return; }
-                auth.register($scope.user).success(function(){
-                    $scope.user = {};
-                    $location.path('/home');
+                if(!$scope.user.email || !$scope.user.password || !$scope.user.firstname || !$scope.user.lastname){ return; }
+                mailbox.getUserList().then(function(userList){
+                    $scope.userExists = false;
+                    var keys = Object.keys(userList.data);
+                    if(keys.indexOf($scope.user.email)!=-1){
+                        $scope.userExists = true;
+                        return;
+                    }
+                    if(!$scope.userExists)
+                        auth.register($scope.user).success(function(){
+                            $scope.user = {};
+                            $location.path('/home');
+                            mailbox.getUserList().success(function(){
+                                keys = Object.keys(userList.data);
+                            });
+                        });
                 });
             };
             $scope.loginUser = function(){
@@ -29,7 +43,7 @@ angular.module('publicApp')
                     $scope.user = {};
                     $location.path('/home');
                 });
-            }
+            };
         }
     ]
 );
